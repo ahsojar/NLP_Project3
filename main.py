a86a45d5da2c040b929b129efea001ac122006f2
Alisha Sojar <ahsojar@dhcp-rhodes-107.redrover.cornell.edu>
2015-11-09 15:38:13 -0500

    Viterbi

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ kaggleTest = "kaggleTest.csv"
 
 unigram_counts = {} 
 bigram_counts = {} #2D bigram counts {word1: {word2: count, word3: count}, word2: {word4: count}}
-bigram_prob = {} #probability of each bigram, 2D
+bigram_prob = {}
+start_tag_counts = {'<S>': 0,'I-PER': 0,'B-PER':0, 'O':0, 'I-LOC':0,'B-LOC':0, 'I-ORG':0, 'B-ORG':0, 'I-MISC':0,'B-MISC':0}
 
 def training(fname):
   with open(fname) as f:
     content = f.readlines()
+    num_sentences = len(content)/3.0
     word_tag_count = {}
     tag_counts= {'<S>': 0,'I-PER': 0,'B-PER':0, 'O':0, 'I-LOC':0,'B-LOC':0, 'I-ORG':0, 'B-ORG':0, 'I-MISC':0,'B-MISC':0}
 
@@ -24,6 +26,8 @@ def training(fname):
       words =['<S>'] + content[l].split()
       pos = ['<S>'] + content[l+1].split()
       tags = ['<S>']+ content[l+2].split()
+      #see which tag this sentence starts with, update count
+      start_tag_counts[tags[1]] += 1
       bigram(tags)
       for w in range(0,len(words)):
         tag_counts[tags[w]] += 1
@@ -32,9 +36,12 @@ def training(fname):
           word_tag_count[words[w]][tags[w]]= 1
         else:
           word_tag_count[words[w]][tags[w]] += 1
+  
   transition_prob = normalize_bigram(unigram_counts, bigram_counts) 
   emission_prob = normalize_emission(word_tag_count, tag_counts)
-  return [transition_prob, emission_prob]
+  start_prob = normalize_startprob(start_tag_counts, num_sentences)
+
+  return [transition_prob, emission_prob, start_prob]
 
 def test(fname,word_tag_count):
   predictions = {
@@ -50,16 +57,6 @@ def test(fname,word_tag_count):
       # words = content[l].split()
       # pos = content[l+1].split()
       # number = content[l+2].split()
-      # lasttag = 0
-      # for w in range(0,len(words)):
-      #   word = words[w]
-      #   if word in word_tag_count:
-      #     tag = word_tag_count[word]
-      #     #select the tag that the word was most used as in the training corpus
-      #     maxtag = max(tag.iteritems(), key=operator.itemgetter(1))[0]
-      #     if maxtag != 'O' or maxtag!='<S>':
-      #       shorten = maxtag[2:]
-      #       predictions[shorten].append(number[w])
   print predictions
  # print_to_file(predictions)
   return predictions
@@ -73,27 +70,28 @@ def bigram(tokens):
     else:
       unigram_counts[word] = 1
   prev_word = '<S>'
+
   for word in tokens:
-    #create 2 dimensional bigrams
     if prev_word in bigram_counts:
-      if word in bigram_counts[prev_word]:
         bigram_counts[prev_word][word] += 1
-      else:
-        bigram_counts[prev_word][word] = 1
     else:
-      bigram_counts[prev_word] = {}
+      bigram_counts[prev_word] = {'<S>': 0,'I-PER': 0,'B-PER':0, 'O':0, 'I-LOC':0,'B-LOC':0, 'I-ORG':0, 'B-ORG':0, 'I-MISC':0,'B-MISC':0}
       bigram_counts[prev_word][word] = 1
 
     prev_word = word
+
+def normalize_startprob(start_tag_counts, num_sentences):
+  for w in start_tag_counts:
+      start_tag_counts[w] = start_tag_counts[w]*(1.0/num_sentences)
+  return start_tag_counts
 
 def normalize_bigram(unigram_counts, bigram_2d):
   for first_word in bigram_counts:
     second_words = bigram_counts[first_word]
     normalized_words = {}
     for w in second_words:
-      if w != '<S>':
-        normalized_words[w] = second_words[w]/ float(unigram_counts[first_word])
-        bigram_prob[first_word] = normalized_words
+      normalized_words[w] = second_words[w]/ float(unigram_counts[first_word])
+      bigram_prob[first_word] = normalized_words
   return bigram_prob
 
 def normalize_emission(emission_counts, tag_counts):
@@ -102,27 +100,6 @@ def normalize_emission(emission_counts, tag_counts):
       emission_counts[all_tags][t] = emission_counts[all_tags][t]*(1.0/tag_counts[t])
   return emission_counts
 
-#the Viterbi algorithm
-def viterbi(hmm, initial_dist, emissions):
-    probs = hmm.emission_dist(emissions[0]) * initial_dist
-    stack = []
-
-    for emission in emissions[1:]:
-        trans_probs = hmm.transition_probs * np.row_stack(probs)
-        max_col_ixs = np.argmax(trans_probs, axis=0)
-        probs = hmm.emission_dist(emission) * trans_probs[max_col_ixs, np.arange(hmm.num_states)]
-
-        stack.append(max_col_ixs)
-
-    state_seq = [np.argmax(probs)]
-
-    while stack:
-        max_col_ixs = stack.pop()
-        state_seq.append(max_col_ixs[state_seq[-1]])
-
-    state_seq.reverse()
-
-    return state_seq
 
 def print_to_file(predictions):
   if os.path.exists(kaggleTest):
@@ -146,14 +123,31 @@ def print_to_file(predictions):
 
       f.write(str(start)+"\n")
  
+def viterbi(obs, states, start_p, trans_p, emit_p):
+  # Initialize base cases (t == 0)
+  V = [{y:(start_p[y] * emit_p[obs[0]][y]) for y in states}]
+  path = {y:[y] for y in states}
+  for y in states:
+    V[0][y] = start_p[y] * emit_p[obs[0]][y]
+    path[y] = [y]
+   # Run Viterbi for t > 0
+  for t in range(1, len(obs)):
+    V.append({})
+    newpath = {}
+    for y in states:
+      (prob, state) = max((V[t-1][y0] * trans_p[y0][y] * emit_p[obs[t]][y], y0) for y0 in states)
+      V[t][y] = prob
+      newpath[y] = path[state] + [y]
+       # Don't need to remember the old paths
+    path = newpath
+  (prob, state) = max((V[t][y], y) for y in states)
+  return (prob, path[state])
+
 hmm = training('train.txt')
 transition_probs = hmm[0]
 emission_probs = hmm[1]
-hmm_transition_probs = np.array([[transition_probs[w][tag] for tag in sorted(transition_probs[w])] for w in sorted(transition_probs)])
-hmm_emission_probs = np.array([[emission_probs[w][tag] for tag in sorted(emission_probs[w])] for w in sorted(emission_probs)]) 
-print hmm_transition_probs
-# hmm_startprobs = np.array([[0.6, 0.4]])
-# wiki_hmm = HMM(wiki_transition_probs, wiki_emission_probs)
-# print(viterbi(wiki_hmm, wiki_initial_dist, wiki_emissions))
-# # test('test.txt', emission_probs)
+start_probs = hmm[2]
+
+print viterbi(['Michael', 'Chang', 'is', 'playing', 'in', 'his', '10th', 'U.S.', 'Open'], ['<S>','I-PER','B-PER', 'O', 'I-LOC','B-LOC', 'I-ORG', 'B-ORG', 'I-MISC','B-MISC'], start_probs, transition_probs, emission_probs )
+
 
